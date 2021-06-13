7acbeb2e1ff5c0880f1fea30efc7d9aaa65ce0d1
Tommaso Pirozzi <tommasopirozzi@hotmail.it>
2021-06-13 12:31:06 +0200

    Using getter method for formControl

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -18,6 +18,18 @@ export class EquationComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public getA(): number {
+    return this.mathForm.value.a;
+    /*
+     Accesso al valore del formControl denominato 'a'.
+     Oppure mathForm.controls.a.value oppure mathForm.value.a oppure this.mathForm.get('a').value;
+    */
+  }
+
+  public getB(): number {
+    return this.mathForm.value.b;
+  }
+
   private randomNumber(): number {
     return Math.floor(Math.random() * 10);
   }
