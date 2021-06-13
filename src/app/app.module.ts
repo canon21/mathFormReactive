bbf845bfb22b5c0adb3563486a9b21dcbf766943
Tommaso Pirozzi <tommasopirozzi@hotmail.it>
2021-06-13 12:14:10 +0200

    Adding reactiveModuleForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { EquationComponent } from './equation/equation.component';
@@ -10,7 +11,8 @@ import { EquationComponent } from './equation/equation.component';
     EquationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
