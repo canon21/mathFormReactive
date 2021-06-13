38b3a0a7f318054bfbf4bb793275ad9babeaa189
Tommaso Pirozzi <tommasopirozzi@hotmail.it>
2021-06-13 23:55:02 +0200

    Hard solution to add helper user for solution. Using directive

--- src/app/answer-high-light.directive.ts
+++ src/app/answer-high-light.directive.ts
@@ -1,10 +1,29 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef  } from '@angular/core';
+import { NgControl } from '@angular/forms';
+import { map, filter} from 'rxjs/operators'
 
 @Directive({
   selector: '[appAnswerHighLight]'
 })
 export class AnswerHighLightDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef, private controlName: NgControl) {
+    console.log(this.el);
+    console.log(this.controlName);
+   }
 
+   ngOnInit() {
+     //Another observable on Form group.
+     this.controlName.control.parent.valueChanges
+      .pipe(
+        map(({a, b, answer}) => Math.abs((a + b - answer) / (a + b)))
+        //filter((value) => value < 0.20 )
+      )
+      .subscribe(value => {
+        if(value < 0.20)
+          this.el.nativeElement.classList.add('close');
+        else
+          this.el.nativeElement.classList.remove('close');
+      });
+   }
 }
